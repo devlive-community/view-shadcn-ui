885e1e8980cf8e8dd9ae683f901a21d7cfd023a2
qianmoQ <shicheng@devlive.org>
2024-03-22 00:13:40 +0800

    [Page] Add sign in

---
+++ src/views/auth/components/SigninForm.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="grid gap-6">
+    <form @submit="onSubmit">
+      <div class="grid gap-2">
+        <FormField v-slot="{ componentField }" name="username">
+          <FormItem>
+            <FormLabel>Username</FormLabel>
+            <FormControl>
+              <Input type="text" v-model="formState.username" v-bind="componentField" placeholder="Please enter your username"/>
+            </FormControl>
+            <FormMessage/>
+          </FormItem>
+        </FormField>
+      </div>
+      <div class="grid gap-2 mt-1">
+        <FormField v-slot="{ componentField }" name="password">
+          <FormItem>
+            <div className='flex items-center justify-between'>
+              <FormLabel>Password</FormLabel>
+              <RouterLink to="" class="text-sm font-medium text-muted-foreground hover:opacity-75">
+                Forgot password?
+              </RouterLink>
+            </div>
+            <FormControl>
+              <Input v-model="formState.password" v-bind="componentField" type="password" placeholder="Please enter your password"/>
+            </FormControl>
+            <FormMessage/>
+          </FormItem>
+        </FormField>
+      </div>
+      <Button class="mt-3 w-full" type="submit" :disabled="loading">
+        <Loader2 v-if="loading" class="mr-2 h-4 w-4 animate-spin"/>
+        Sign in
+      </Button>
+    </form>
+    <div class="relative my-2">
+      <div class="absolute inset-0 flex items-center">
+        <span class="w-full border-t"/>
+      </div>
+      <div class="relative flex justify-center text-xs uppercase">
+          <span class="bg-background px-2 text-muted-foreground">
+            Or continue with
+          </span>
+      </div>
+    </div>
+    <div class="flex items-center gap-2">
+      <Button variant='outline' class="w-full">
+        <Github class="mr-2 h-4 w-4"/>
+        GitHub
+      </Button>
+      <Button variant='outline' class="w-full">
+        <Facebook class="mr-2 h-4 w-4"/>
+        Facebook
+      </Button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { User } from '@/views/auth/User'
+import { Input } from '@/components/ui/input'
+import { Facebook, Github, Loader2 } from 'lucide-vue-next'
+import * as z from 'zod'
+import { toTypedSchema } from '@vee-validate/zod'
+import { useForm } from 'vee-validate'
+import { Button } from '@/components/ui/button'
+
+export default defineComponent({
+  name: 'SigninForm',
+  components: {
+    Button,
+    Input,
+    FormControl, FormField, FormItem, FormLabel, FormMessage,
+    Loader2, Github, Facebook
+  },
+  setup()
+  {
+    let loading = ref(false)
+    const formState = ref<User>({username: undefined, password: undefined})
+    const validator = z
+        .object({
+          username: z.string({required_error: 'Username is required'})
+              .min(2, 'Username must be at least 2 characters')
+              .max(20, 'Username must be at most 20 characters'),
+          password: z.string({required_error: 'Password is required'})
+              .min(6, 'Password must be at least 6 characters')
+              .max(20, 'Password must be at most 20 characters')
+        })
+
+    const {handleSubmit} = useForm({
+      validationSchema: toTypedSchema(validator)
+    })
+
+    const onSubmit = handleSubmit(() => {
+      loading.value = true
+      setTimeout(() => {
+        loading.value = false
+      }, 3000)
+    })
+
+    return {
+      loading,
+      formState,
+      onSubmit
+    }
+  }
+})
+</script>
