ebb23dcaf9e4bc26e3ff7eed993b46522902153b
qianmoQ <shicheng@devlive.org>
2024-03-22 21:45:01 +0800

    [Page] [Layout] Add sidebar

---
+++ src/model/Navigation.ts
@@ -0,0 +1,8 @@
+export interface NavigationModel
+{
+    title?: string
+    label?: string
+    href?: string
+    icon?: any
+    children?: NavigationModel[]
+}
---
+++ src/services/Navigation.ts
@@ -0,0 +1,12 @@
+import { NavigationModel } from '@/model/Navigation'
+
+class NavigationService
+{
+    getNavigation(): NavigationModel[]
+    {
+        return []
+    }
+
+}
+
+export default new NavigationService()
