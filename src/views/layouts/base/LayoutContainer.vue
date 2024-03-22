2b8517afab75debfbfa71b65b7fc8b1c3402b763
qianmoQ <shicheng@devlive.org>
2024-03-22 22:48:30 +0800

    [Page] [Layout] Add header

--- src/views/layouts/base/LayoutContainer.vue
+++ src/views/layouts/base/LayoutContainer.vue
@@ -1,7 +1,10 @@
 <template>
   <LayoutSidebar @changeCollapsed="isCollapsed = $event"/>
-  <div :class="cn(`overflow-auto space-y-2 p-2 ${isCollapsed ? 'md:ml-14' : 'md:ml-64'}`)">
-    <RouterView/>
+  <div :class="cn(`overflow-auto ${isCollapsed ? 'md:ml-14' : 'md:ml-64'}`)">
+    <LayoutHeader/>
+    <div class="p-2 space-y-2">
+      <RouterView/>
+    </div>
   </div>
 </template>
 
@@ -9,10 +12,11 @@
 import { defineComponent } from 'vue'
 import LayoutSidebar from '@/views/layouts/base/components/LayoutSidebar.vue'
 import { cn } from '@/lib/utils'
+import LayoutHeader from '@/views/layouts/base/components/LayoutHeader.vue'
 
 export default defineComponent({
   name: 'LayoutContainer',
-  components: {LayoutSidebar},
+  components: {LayoutHeader, LayoutSidebar},
   setup()
   {
     return {
---
+++ src/views/layouts/base/components/LayoutHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex h-[var(--header-height)] flex-none items-center gap-4 bg-background p-4 md:px-8 border-b">
+    I'm LayoutHeader
+  </div>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Button } from '@/components/ui/button'
+
+export default defineComponent({
+  name: 'LayoutHeader',
+  components: {Button}
+})
+</script>
