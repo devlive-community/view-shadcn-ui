cb36b4c31279fdc6946e457bd62ad875094edd7f
qianmoQ <shicheng@devlive.org>
2024-03-22 22:56:43 +0800

    [Page] [Layout] Fixed router link

--- src/views/layouts/base/components/components/NavigationClosed.vue
+++ src/views/layouts/base/components/components/NavigationClosed.vue
@@ -1,10 +1,12 @@
 <template>
-  <Button as="a" size="sm" class="justify-start text-wrap rounded-none h-10 w-full border-l border-l-slate-500 px-2" :href="navigator?.href"
-          :variant="$route.path === `${navigator?.href}` ? 'secondary' : 'ghost'">
-    <component :is="navigator?.icon"/>
-    <div class="ml-2">{{ navigator?.title }}</div>
-    <div v-if="navigator?.label" class="ml-2 rounded-lg bg-primary px-2 mt-0.5 text-[0.625rem] text-primary-foreground">{{ navigator?.label }}</div>
-  </Button>
+  <RouterLink :to="navigator?.href">
+    <Button as="a" size="sm" class="justify-start text-wrap rounded-none h-10 w-full border-l border-l-slate-500 px-2"
+            :variant="$route.path === `${navigator?.href}` ? 'secondary' : 'ghost'">
+      <component :is="navigator?.icon"/>
+      <div class="ml-2">{{ navigator?.title }}</div>
+      <div v-if="navigator?.label" class="ml-2 rounded-lg bg-primary px-2 mt-0.5 text-[0.625rem] text-primary-foreground">{{ navigator?.label }}</div>
+    </Button>
+  </RouterLink>
 </template>
 
 <script lang="ts">
--- src/views/layouts/base/components/components/NavigationOpened.vue
+++ src/views/layouts/base/components/components/NavigationOpened.vue
@@ -1,17 +1,19 @@
 <template>
-  <TooltipProvider>
-    <Tooltip :delay-duration="0">
-      <TooltipTrigger asChild>
-        <Button as="a" size="icon" class="h-12 w-12" :href="navigator?.href" :variant="$route.path === `${navigator?.href}` ? 'secondary' : 'ghost'">
-          <component :is="navigator?.icon"/>
-        </Button>
-      </TooltipTrigger>
-      <TooltipContent side="right" class="flex items-center gap-4">
-        {{ navigator?.title }}
-        <span v-if="navigator?.label" class="ml-auto text-muted-foreground">{{ navigator?.label }}</span>
-      </TooltipContent>
-    </Tooltip>
-  </TooltipProvider>
+  <RouterLink :to="navigator?.href">
+    <TooltipProvider>
+      <Tooltip :delay-duration="0">
+        <TooltipTrigger asChild>
+          <Button as="a" size="icon" class="h-12 w-12" :variant="$route.path === `${navigator?.href}` ? 'secondary' : 'ghost'">
+            <component :is="navigator?.icon"/>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent side="right" class="flex items-center gap-4">
+          {{ navigator?.title }}
+          <span v-if="navigator?.label" class="ml-auto text-muted-foreground">{{ navigator?.label }}</span>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  </RouterLink>
 </template>
 
 <script lang="ts">
