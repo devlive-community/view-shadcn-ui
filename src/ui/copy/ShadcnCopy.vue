2254c7a0e78736d0f28b6a9cfad779b593d1acc2
qianmoQ <shicheng@devlive.org>
2024-10-15 20:28:45 +0800

    [Components] [Copy] Support tooltip

--- src/ui/copy/ShadcnCopy.vue
+++ src/ui/copy/ShadcnCopy.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="relative inline-flex items-center">
-    <slot v-if="$slots.icon" name="icon"/>
+    <div v-if="$slots.icon" class="cursor-pointer" @click="onCopy">
+      <slot name="icon"/>
+    </div>
     <ShadcnIcon v-else class="cursor-pointer" :icon="icon" @click="onCopy"/>
+
+    <transition v-if="tooltip" name="fade">
+      <div v-if="showTooltip" class="absolute bottom-full mb-2 left-1/2 transform -translate-x-1/2 bg-gray-700 text-white text-xs rounded p-1.5">
+        {{ tooltipMessage }}
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -9,27 +17,37 @@
 import ShadcnIcon from '@/ui/icon'
 import { ref } from 'vue'
 
-const props = defineProps<{
+const props = withDefaults(defineProps<{
   text: string
-}>()
+  tooltip?: boolean
+}>(), {
+  tooltip: true
+})
 
 const emit = defineEmits(['on-success', 'on-failed'])
 
 const icon = ref<string>('Copy')
+const showTooltip = ref<boolean>(false)
+const tooltipMessage = ref<string>('Copied!')
 
 const onCopy = async () => {
   try {
     await navigator.clipboard
                    .writeText(props.text)
     icon.value = 'Check'
+    tooltipMessage.value = 'Copied!'
+    showTooltip.value = true
     emit('on-success')
   }
   catch (error) {
+    tooltipMessage.value = 'Copy failed!'
+    showTooltip.value = true
     emit('on-failed')
   }
   finally {
     setTimeout(() => {
       icon.value = 'Copy'
+      showTooltip.value = false
     }, 500)
   }
 }
--- src/views/pages/components/CopyHome.vue
+++ src/views/pages/components/CopyHome.vue
@@ -15,11 +15,19 @@
         </ShadcnCopy>
       </div>
     </ShadcnCard>
+
+    <ShadcnCard title="Tooltip">
+      <div class="flex items-center justify-center h-32 p-3 space-x-2">
+        <ShadcnCopy :text="text"/>
+        <ShadcnCopy :text="text" :tooltip="false"/>
+      </div>
+    </ShadcnCard>
   </div>
 </template>
 
 <script setup lang="ts">
 import { HomeIcon } from 'lucide-vue-next'
+import ShadcnCopy from '@/ui/copy'
 
 const text = 'Hello View Shadcn UI'
 </script>
