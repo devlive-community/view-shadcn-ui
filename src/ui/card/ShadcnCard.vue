596d696a3d4d473a75192d5ca7c7a052a9b39649
qianmoQ <shicheng@devlive.org>
2024-10-30 15:10:39 +0800

    refactor(card): remove shadcn dep

--- src/ui/card/ShadcnCard.vue
+++ src/ui/card/ShadcnCard.vue
@@ -1,46 +1,46 @@
 <template>
-  <Card :class="cn('relative rounded-sm', computedShadow,
-                   !$slots.content ? 'min-h-32' : '')">
+  <div :class="cn('bg-white border rounded-sm', computedShadow,
+                  !$slots.content ? 'min-h-32' : '')">
     <div v-if="loading" class="absolute inset-0 bg-gray-100 bg-opacity-50 flex items-center justify-center z-10">
-      <Loader2 class="mr-2 animate-spin text-center text-blue-300"/>
+      <ShadcnIcon icon="Loader2" class="h-5 w-5 animate-spin"/>
     </div>
     <div v-else class="relative">
-      <CardHeader v-if="$slots.title || title" :class="cn('flex flex-row items-center justify-between border-b p-2')">
+      <div v-if="$slots.title || title"
+           :class="cn('flex flex-row items-center justify-between border-b p-2')">
         <div class="grid gap-2">
-          <CardTitle>
+          <h3 class="text-lg font-semibold leading-none tracking-tight">
             <span v-if="title">{{ title }}</span>
             <slot v-else name="title"/>
-          </CardTitle>
+          </h3>
           <div v-if="$slots.description || description">
-            <CardDescription>
+            <p class="text-sm text-muted-foreground">
               <span v-if="description">{{ description }}</span>
               <slot v-else name="description"/>
-            </CardDescription>
+            </p>
           </div>
         </div>
         <div v-if="$slots.extra">
           <slot name="extra"/>
         </div>
-      </CardHeader>
-      <CardContent v-if="$slots.content" class="p-0 min-h-32">
+      </div>
+      <div v-if="$slots.content" class="p-0 min-h-32">
         <slot name="content"/>
-      </CardContent>
-      <CardContent v-else class="p-0">
+      </div>
+      <div v-else class="p-0">
         <slot/>
-      </CardContent>
-      <CardFooter v-if="$slots.footer" class="p-0">
+      </div>
+      <div v-if="$slots.footer" class="p-0">
         <slot name="footer"/>
-      </CardFooter>
+      </div>
     </div>
-  </Card>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ref, watchEffect } from 'vue'
 import { cn } from '@/lib/utils'
-// import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
-import { Loader2 } from 'lucide-vue-next'
 import { Shadow } from '@/ui/enum/Shadow.ts'
+import ShadcnIcon from '@/ui/icon'
 
 const props = defineProps<{
   title?: string
