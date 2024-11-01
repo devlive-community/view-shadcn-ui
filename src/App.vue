b42f4b434a36b10a4b30aa0752ec0086891d6efe
qianmoQ <shicheng@devlive.org>
2024-11-01 19:13:47 +0800

    feat(button): support router mode

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
-<div class="h-screen">
-  <ShadcnException type="500">
-    <template #icon>
-      <ShadcnIcon icon="TriangleAlert" class="text-gray-400" :size="80"/>
-    </template>
-    <template #actions>
-      <ShadcnButton type="primary">Home</ShadcnButton>
-    </template>
-  </ShadcnException>
-</div>
+  <div class="h-screen">
+    <ShadcnException type="500">
+      <template #icon>
+        <ShadcnIcon icon="TriangleAlert" class="text-gray-400" :size="80"/>
+      </template>
+      <template #actions>
+        <ShadcnButton type="primary" to="https://shadcn.com">Home</ShadcnButton>
+        <ShadcnButton type="primary" submit>Home</ShadcnButton>
+      </template>
+    </ShadcnException>
+  </div>
 </template>
 
 <script setup lang="ts">
