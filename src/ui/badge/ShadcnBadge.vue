65fa5de2fbc31933fceb44817c3e8b70ed764d63
qianmoQ <shicheng@devlive.org>
2024-11-03 15:23:03 +0800

    fix(badge): fix default value position

--- src/ui/badge/ShadcnBadge.vue
+++ src/ui/badge/ShadcnBadge.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="relative inline-block">
+  <div :class="['relative', $slots.default && 'inline-block']">
     <slot/>
 
-    <div :class="cn('absolute top-0 right-0 rounded-full text-white text-xs font-medium',
+    <div :class="cn('top-0 right-0 rounded-full w-fit text-white text-xs font-medium',
                     dot ? 'p-1' : 'px-1.5 py-0.5',
-                    $slots.default && 'translate-x-1/2 -translate-y-1/2',
+                    $slots.default && 'absolute translate-x-1/2 -translate-y-1/2',
                     ButtonBackgroundType[type]
           )">
       <slot name="text">
