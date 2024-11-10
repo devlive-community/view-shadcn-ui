c09ecbd93f8f507d4dc0a1f6849755d5be7b1715
qianmoQ <shicheng@devlive.org>
2024-11-10 12:09:05 +0800

    feat(divider): optimize the left and right layout

--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,21 @@
 <template>
-  <ShadcnSpace wrap>
-    <ShadcnRow gutter="10">
-      <ShadcnCol v-for="i in 40" :key="i" class="bg-blue-200 h-12">
-        <div class="flex items-center justify-center h-full">
-          {{ i }}
-        </div>
-      </ShadcnCol>
-    </ShadcnRow>
+  <div class="p-32 space-y-2">
+    <div>Horizontal</div>
+    <ShadcnDivider type="horizontal"/>
+    <div class="flex space-x-2">
+      <div>Vertical</div>
+      <ShadcnDivider type="vertical"/>
+      <a href="#">Link</a>
+      <ShadcnDivider type="vertical"/>
+      <div>Vertical</div>
+    </div>
 
-    <ShadcnRow>
-      <ShadcnCol class="bg-blue-200 h-12"></ShadcnCol>
-      <ShadcnCol class="bg-blue-300 h-12"></ShadcnCol>
-      <ShadcnCol class="bg-blue-400 h-12"></ShadcnCol>
-      <ShadcnCol class="bg-blue-500 h-12"></ShadcnCol>
-    </ShadcnRow>
+    <ShadcnDivider orientation="left">Left</ShadcnDivider>
+    <ShadcnDivider orientation="center">Center</ShadcnDivider>
+    <ShadcnDivider orientation="right">Right</ShadcnDivider>
 
-    <ShadcnRow>
-      <ShadcnCol class="bg-blue-200 h-12" span="2"></ShadcnCol>
-      <ShadcnCol class="bg-blue-300 h-12" span="1"></ShadcnCol>
-      <ShadcnCol class="bg-blue-400 h-12" span="3"></ShadcnCol>
-      <ShadcnCol class="bg-blue-500 h-12" span="1"></ShadcnCol>
-    </ShadcnRow>
-
-    <ShadcnRow>
-      <ShadcnCol class="bg-blue-200 h-12" order="2" span="3">1 | order 2</ShadcnCol>
-      <ShadcnCol class="bg-blue-300 h-12" order="1" span="3">2 | order 1</ShadcnCol>
-      <ShadcnCol class="bg-blue-400 h-12" order="3" span="3">3 | order 3</ShadcnCol>
-      <ShadcnCol class="bg-blue-500 h-12" order="4" span="3">4 | order 4</ShadcnCol>
-    </ShadcnRow>
-
-    <ShadcnText type="h2">Align</ShadcnText>
-    <ShadcnRow align="top">
-      <ShadcnCol class="bg-blue-200 h-16"></ShadcnCol>
-      <ShadcnCol class="bg-blue-300 h-14"></ShadcnCol>
-      <ShadcnCol class="bg-blue-400 h-10"></ShadcnCol>
-      <ShadcnCol class="bg-blue-500 h-14"></ShadcnCol>
-    </ShadcnRow>
-    <ShadcnRow align="middle">
-      <ShadcnCol class="bg-blue-200 h-16"></ShadcnCol>
-      <ShadcnCol class="bg-blue-300 h-14"></ShadcnCol>
-      <ShadcnCol class="bg-blue-400 h-10"></ShadcnCol>
-      <ShadcnCol class="bg-blue-500 h-14"></ShadcnCol>
-    </ShadcnRow>
-    <ShadcnRow align="bottom">
-      <ShadcnCol class="bg-blue-200 h-16"></ShadcnCol>
-      <ShadcnCol class="bg-blue-300 h-14"></ShadcnCol>
-      <ShadcnCol class="bg-blue-400 h-10"></ShadcnCol>
-      <ShadcnCol class="bg-blue-500 h-14"></ShadcnCol>
-    </ShadcnRow>
-
-    <ShadcnText type="h2">Justify</ShadcnText>
-    <ShadcnRow justify="start">
-      <ShadcnCol class="bg-blue-200 h-16"></ShadcnCol>
-      <ShadcnCol class="bg-blue-300 h-14"></ShadcnCol>
-      <ShadcnCol class="bg-blue-400 h-10"></ShadcnCol>
-      <ShadcnCol class="bg-blue-500 h-14"></ShadcnCol>
-    </ShadcnRow>
-    <ShadcnRow justify="center">
-      <ShadcnCol class="bg-blue-200 h-16"></ShadcnCol>
-      <ShadcnCol class="bg-blue-300 h-14"></ShadcnCol>
-      <ShadcnCol class="bg-blue-400 h-10"></ShadcnCol>
-      <ShadcnCol class="bg-blue-500 h-14"></ShadcnCol>
-    </ShadcnRow>
-    <ShadcnRow justify="end">
-      <ShadcnCol class="bg-blue-200 h-16"></ShadcnCol>
-      <ShadcnCol class="bg-blue-300 h-14"></ShadcnCol>
-      <ShadcnCol class="bg-blue-400 h-10"></ShadcnCol>
-      <ShadcnCol class="bg-blue-500 h-14"></ShadcnCol>
-    </ShadcnRow>
-    <ShadcnRow justify="between">
-      <ShadcnCol class="bg-blue-200 h-16"></ShadcnCol>
-      <ShadcnCol class="bg-blue-300 h-14"></ShadcnCol>
-      <ShadcnCol class="bg-blue-400 h-10"></ShadcnCol>
-      <ShadcnCol class="bg-blue-500 h-14"></ShadcnCol>
-    </ShadcnRow>
-  </ShadcnSpace>
+    <ShadcnDivider dashed />
+  </div>
 </template>
 
 <script setup lang="ts">
--- src/ui/divider/ShadcnDivider.vue
+++ src/ui/divider/ShadcnDivider.vue
@@ -1,33 +1,58 @@
 <template>
-  <div :class="cn(type === 'vertical' ? 'border-l' : 'flex items-center w-full')">
-    <div v-if="orientation !== 'left'"
-         :class="cn(type === 'vertical' ? 'bg-gray-300' : 'flex-grow border-t border-gray-300',
-                    dashed && 'border-dashed')"/>
+  <div :class="cn(type === 'vertical' ? 'border-l' : 'flex items-center w-full relative pr-8')">
+    <!-- Left dividing line -->
+    <div v-if="text && orientation === 'left'" class="w-8 border-t border-gray-100"
+         :class="dashed && 'border-dashed'"/>
+    <div v-else-if="orientation !== 'left'"
+         :class="cn(
+           type === 'vertical' ? 'bg-gray-100' : 'flex-grow border-t border-gray-100',
+           dashed && 'border-dashed',
+           orientation === 'right' && 'flex-1'
+         )"/>
 
-    <div :class="cn(
-           orientation === 'left' && (type === 'vertical' ? 'order-first mb-2' : 'order-first mr-4'),
-                 orientation === 'right' && (type === 'vertical' ? 'order-last mt-2' : 'order-last ml-4'),
-                 orientation === 'center' && (type === 'vertical' ? 'my-2' : 'mx-6')
+    <!-- A small short line on the left -->
+    <div v-if="(text || $slots.default) && orientation === 'left' && type === 'horizontal'" class="w-8 mr-4 border-t border-gray-100"
+         :class="dashed && 'border-dashed'"/>
+
+    <!-- Text or slot content -->
+    <span v-if="text"
+          :class="cn(
+            orientation === 'left' && (type === 'vertical' ? 'order-first mb-2' : 'mx-4'),
+            orientation === 'right' && (type === 'vertical' ? 'order-last mt-2' : 'order-last mx-4'),
+            orientation === 'center' && (type === 'vertical' ? 'my-2' : 'mx-6'),
+            'text-sm text-gray-500',
+            (orientation === 'right' && type === 'horizontal') && 'mr-3'
+          )">
+      {{ text }}
+    </span>
+    <div v-else
+         :class="cn(
+           orientation === 'left' && (type === 'vertical' ? 'order-first mb-2' : 'mr-4'),
+           orientation === 'right' && (type === 'vertical' ? 'order-last mt-2' : 'order-last mx-4'),
+           orientation === 'center' && (type === 'vertical' ? 'my-2' : 'mx-6'),
          )">
-      <slot v-if="$slots.default"/>
-      <span v-else-if="text">{{ text }}</span>
+      <slot/>
     </div>
 
-    <div v-if="orientation !== 'right'"
-         :class="cn(type === 'vertical' ? 'bg-gray-300' : 'flex-grow border-t border-gray-300',
-                    dashed && 'border-dashed')"/>
+    <!-- Dividing line on the right -->
+    <div :class="cn(
+           type === 'vertical' ? 'bg-gray-100' : 'flex-grow border-t border-gray-100',
+           dashed && 'border-dashed',
+           orientation === 'right' && 'flex-1'
+         )"/>
+
+    <!-- A small short line on the right -->
+    <div v-if="(text || $slots.default) && orientation === 'right' && type === 'horizontal'"
+         class="absolute right-0 top-1/2 -translate-y-1/2 w-8 border-t border-gray-100"
+         :class="dashed && 'border-dashed'"/>
   </div>
 </template>
 
 <script setup lang="ts">
-import { cn } from '@/lib/utils.ts'
+import { cn } from '@/lib/utils'
+import { DividerProps } from '@/ui/divider/types.ts'
 
-withDefaults(defineProps<{
-  text?: string
-  type?: 'horizontal' | 'vertical'
-  orientation?: 'left' | 'center' | 'right'
-  dashed?: boolean
-}>(), {
+withDefaults(defineProps<DividerProps>(), {
   type: 'horizontal',
   dashed: false
 })
