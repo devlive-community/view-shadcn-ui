b6610f18ecbcc4b9f91581d04fc45064bfeac334
qianmoQ <shicheng@devlive.org>
2024-11-13 01:35:28 +0800

    fix(tree): fix the abnormal hover and selected styles in lazy loading

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,58 @@
 <template>
-  <ShadcnCard title="Spin">
-    <div class="relative min-h-[200px]">
-      <ShadcnSpin fixed/>
-
-      <ShadcnAlert type="primary">Alert</ShadcnAlert>
-      <ShadcnAlert type="success">Alert</ShadcnAlert>
-      <ShadcnAlert type="warning">Alert</ShadcnAlert>
-      <ShadcnAlert type="error">Alert</ShadcnAlert>
+  <div class="relative min-h-screen w-64 overflow-auto">
+    <div class="w-64 inline-block">
+      <ShadcnTree v-model="value" :data="data" :loadData="loadNodeData">
+        <template #label="{ node }">
+            <span class="text-xs font-normal text-gray-900">
+              {{ node.label }}
+            </span>
+        </template>
+      </ShadcnTree>
     </div>
-  </ShadcnCard>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { reactive, ref } from 'vue'
+
+const value = ref([])
+const data = reactive<[]>([
+  {
+    value: 1,
+    label: 'Parent Node 1',
+    isLeaf: false,
+    children: []
+  },
+  {
+    value: 2,
+    label: 'Parent Node 2',
+    children: [
+      { value: '2.1', label: 'Child Node 2.1', children: [] }
+    ]
+  }
+])
+
+const generateChildNodes = (parentValue: string, level: number = 1, maxLevel: number = 3): any[] => {
+  if (level >= maxLevel) {
+    return []
+  }
+
+  const count = Math.floor(Math.random() * 3) + 1
+  return Array.from({ length: count }, (_, index) => {
+    const value = `${ parentValue }.${ index + 1 }`
+    return {
+      value,
+      label: `Node\u00A0${ value }`,
+      isLeaf: level === maxLevel - 1,
+      children: []
+    }
+  })
+}
+const loadNodeData = (item: any, callback: (children: any[]) => void) => {
+  setTimeout(() => {
+    const level = 0
+    const children = generateChildNodes(item.value, level)
+    callback(children)
+  }, 1000)
+}
 </script>
