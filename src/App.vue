588b8dd889d8846068c262c984c1e8dc5608043c
qianmoQ <shicheng@devlive.org>
2024-11-13 13:55:08 +0800

    fix(input): fixed textarea border

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,9 @@
 <template>
-  <ShadcnTab>
-    <ShadcnTabItem label="Tab 1" value="Tab 1">
-      <p>Tab 1 content</p>
-    </ShadcnTabItem>
-    <ShadcnTabItem label="Tab 2" value="Tab 2">
-      <p>Tab 2 content</p>
-    </ShadcnTabItem>
-    <ShadcnTabItem value="Tab 3" @on-click="console.log('Tab 3 clicked')">
-      <template #label>
-        <div class="flex items-center space-x-1">
-          <ShadcnIcon icon="Save"/>
-          <span>Label Slot</span>
-        </div>
-      </template>
-      <p>Tab 3 content</p>
-    </ShadcnTabItem>
-  </ShadcnTab>
+  <ShadcnInput v-model="input" placeholder="Input" type="textarea"/>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+
+const input = ref('Hello View Shadcn UI')
 </script>
