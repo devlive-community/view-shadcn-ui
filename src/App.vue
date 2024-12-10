a3728f78982ef350c3bfe01700b551e6c135e4d1
qianmoQ <shicheng@devlive.org>
2024-12-10 21:47:55 +0800

    fix(workflow): fixed the display problem after data configuration exceeded

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,86 @@
 <template>
-  <div class="p-32 space-y-3">
-    <ShadcnSelect v-model="selectedLocale" :options="defaultSelectOptions" @on-change="setLocaleLanguage"/>
-    <ShadcnUpload action="//jsonplaceholder.typicode.com/posts/"/>
-  </div>
+  <ShadcnWorkflowEditor v-model="workflowState"
+                        :categories="categories"
+                        :nodes="nodes"
+                        :connections="[]"
+                        :search-text="searchText">
+  </ShadcnWorkflowEditor>
 </template>
 
 <script setup lang="ts">
-import {ref} from 'vue'
-import {addLocale, setLocale} from "@/utils/locale.ts";
+import { ref } from 'vue'
 
-const selectedLocale = ref('en')
-const defaultSelectOptions = [
-  {label: 'ZH-CN', value: 'zh -CN'},
-  {label: 'en', value: 'en'},
-  {label: 'ru', value: 'ru-RU'}
-]
-
-const ruRU = {
-  upload: {
-    text: {
-      placeholder: '<span class="text-blue-600">Нажмите для загрузки</span> или перетащите файл сюда',
-      description: 'Поддерживается одиночная или массовая загрузка.',
-      success: 'Загрузка выполнена успешно',
-      error: 'Ошибка загрузки'
-    }
+const categories = [
+  {
+    label: 'Input Node',
+    value: 'input'
+  },
+  {
+    label: 'Output Node',
+    value: 'output'
+  },
+  {
+    label: 'Transform Node',
+    value: 'transform'
   }
-}
+]
+const searchText = ref('')
+const workflowState = ref({
+  nodes: [],
+  connections: []
+})
 
-addLocale('ru-RU', ruRU)
-
-const setLocaleLanguage = (value: any) => {
-  setLocale(value.value)
-}
+const nodes: any[] = [
+  {
+    id: 'start',
+    label: 'Start',
+    category: 'input',
+    position: { x: 0, y: 0 },
+    configure: [
+      {
+        field: 'name',
+        label: 'Name',
+        type: 'input',
+        description: 'This is a long description, this is a long description, this is a long description, this is a long description',
+        required: true
+      },
+      {
+        field: 'name1',
+        label: 'Password',
+        type: 'password',
+        rules: [
+          { required: true, message: 'Please input password!' }
+        ],
+        required: true
+      }
+    ],
+    description: 'Job start node',
+    ports: [
+      { id: 'out1', type: 'output', label: 'Output', required: true, message: 'Input is required' }
+    ]
+  },
+  {
+    id: 'end',
+    label: 'End',
+    category: 'output',
+    description: 'Job end node',
+    position: { x: 0, y: 0 },
+    ports: [
+      { id: 'in1', type: 'input', label: 'Input', required: true }
+    ]
+  },
+  {
+    id: 'process',
+    label: 'Process',
+    category: 'transform',
+    description: 'Job transform node',
+    position: { x: 0, y: 0 },
+    ports: [
+      { id: 'in12', type: 'input', label: 'Input 1', required: true },
+      { id: 'in22', type: 'input', label: 'Input 2', required: true },
+      { id: 'out1', type: 'output', label: 'Output 1' },
+      { id: 'out2', type: 'output', label: 'Output 2' }
+    ]
+  }
+]
 </script>
