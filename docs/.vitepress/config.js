624204af47ce15c475cd51aabbc2be0e7a3db224
qianmoQ <shicheng@devlive.org>
2024-12-17 12:12:39 +0800

    refactor(doc): refactoring menu navigation layout

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,3 +1,17 @@
+function createSidebarItem(item) {
+    return {
+        text: `<span class="sidebar-text-wrapper">
+            <span class="sidebar-item">
+            ${item.icon ? `<img src="${item.icon}" alt="icon" />` : ''}
+            <span class="sidebar-text-wrapper">
+              ${item.text}
+              ${item.version ? `<span class="version-badge VPBadge tip">${item.version}</span>` : ''}
+            </span>
+           </span>`,
+        link: item.link
+    }
+}
+
 export default {
     title: 'View Shadcn UI',
     description: 'View Shadcn UI is a Vue3 component library built on Tailwind CSS.',
@@ -48,7 +62,7 @@ export default {
             },
             {
                 text: 'Components',
-                link: '/components',
+                link: '/components/base/button',
                 activeMatch: '/components'
             },
             {
@@ -109,140 +123,145 @@ export default {
             '/components/': [
                 (() => {
                     const items = [
-                        {text: 'Language <span class="VPBadge tip">2024.5.2</span>', link: 'utility/language'}
+                        {text: 'Language', link: 'utility/language', version: '2024.5.2'},
                     ]
 
                     return {
                         text: `Utility [ ${items.length} ]`,
                         base: '/components/',
                         collapsed: false,
-                        items: items
+                        items: items.map(item => createSidebarItem(item))
                     }
                 })(),
                 (() => {
                     const items = [
-                        {text: 'Button <span class="VPBadge tip">2024.1.1</span>', link: 'button'},
-                        {text: 'Icon <span class="VPBadge tip">2024.1.1</span>', link: 'icon'},
-                        {text: 'Copy <span class="VPBadge tip">2024.1.1</span>', link: 'copy'},
-                        {text: 'Watermark <span class="VPBadge tip">2024.2.0</span>', link: 'watermark'},
-                        {text: 'Highlight <span class="VPBadge tip">2024.2.0</span>', link: 'highlight'},
-                        {text: 'Text <span class="VPBadge tip">2024.3.0</span>', link: 'text'},
-                        {text: 'Gradient Text <span class="VPBadge tip">2024.3.0</span>', link: 'gradient-text'},
+                        {text: 'Button', link: 'base/button', icon: '/components/base/button.svg', version: '2024.1.1'},
+                        {text: 'Icon', link: 'base/icon', icon: '/components/base/icon.svg', version: '2024.1.1'},
+                        {text: 'Copy', link: 'base/copy', icon: '/components/base/copy.svg', version: '2024.1.1'},
+                        {text: 'Watermark', link: 'base/watermark', icon: '/components/base/watermark.svg', version: '2024.2.0'},
+                        {text: 'Highlight', link: 'base/highlight', icon: '/components/base/highlight.svg', version: '2024.2.0'},
+                        {text: 'Text', link: 'base/text', icon: '/components/base/text.svg', version: '2024.3.0'},
+                        {text: 'Gradient Text', link: 'base/gradient-text', icon: '/components/base/gradient-text.svg', version: '2024.3.0'},
                     ]
 
                     return {
                         text: `Base [ ${items.length} ]`,
                         base: '/components/',
                         collapsed: false,
-                        items: items
+                        items: items.map(item => createSidebarItem(item))
                     }
                 })(),
                 (() => {
                     const items = [
-                        {text: 'Card <span class="VPBadge tip">2024.1.1</span>', link: 'card'},
-                        {text: 'Row / Col <span class="VPBadge tip">2024.1.2</span>', link: 'row-col'},
-                        {text: 'Divider <span class="VPBadge tip">2024.1.2</span>', link: 'divider'},
-                        {text: 'Ellipsis <span class="VPBadge tip">2024.1.2</span>', link: 'ellipsis'},
-                        {text: 'Avatar <span class="VPBadge tip">2024.1.2</span>', link: 'avatar'},
-                        {text: 'Space <span class="VPBadge tip">2024.1.2</span>', link: 'space'},
-                        {text: 'Skeleton <span class="VPBadge tip">2024.1.2</span>', link: 'skeleton'},
-                        {text: 'Layout <span class="VPBadge tip">2024.1.2</span>', link: 'layout'},
-                        {text: 'Collapse <span class="VPBadge tip">2024.2.0</span>', link: 'collapse'},
-                        {text: 'Global Footer <span class="VPBadge tip">2024.2.0</span>', link: 'global-footer'},
-                        {text: 'Toolbar Footer <span class="VPBadge tip">2024.2.0</span>', link: 'toolbar-footer'},
+                        {text: 'Card', link: 'layout/card', icon: '/components/layout/card.svg', version: '2024.1.1'},
+                        {text: 'Row / Col', link: 'layout/row-col', icon: '/components/layout/row-col.svg', version: '2024.1.2'},
+                        {text: 'Divider', link: 'layout/divider', icon: '/components/layout/divider.svg', version: '2024.1.2'},
+                        {text: 'Ellipsis', link: 'layout/ellipsis', icon: '/components/layout/ellipsis.svg', version: '2024.1.2'},
+                        {text: 'Avatar', link: 'layout/avatar', icon: '/components/layout/avatar.svg', version: '2024.1.2'},
+                        {text: 'Space', link: 'layout/space', icon: '/components/layout/space.svg', version: '2024.1.2'},
+                        {text: 'Skeleton', link: 'layout/skeleton', icon: '/components/layout/skeleton.svg', version: '2024.1.2'},
+                        {text: 'Layout', link: 'layout/layout', icon: '/components/layout/layout.svg', version: '2024.1.2'},
+                        {text: 'Collapse', link: 'layout/collapse', icon: '/components/layout/collapse.svg', version: '2024.2.0'},
+                        {text: 'Global Footer', link: 'layout/global-footer', icon: '/components/layout/global-footer.svg', version: '2024.2.0'},
+                        {text: 'Toolbar Footer', link: 'layout/toolbar-footer', icon: '/components/layout/toolbar-footer.svg', version: '2024.2.0'}
                     ]
+
                     return {
                         text: `Layout [ ${items.length} ]`,
                         base: '/components/',
                         collapsed: false,
-                        items: items
+                        items: items.map(item => createSidebarItem(item))
                     }
                 })(),
                 (() => {
                     const items = [
-                        {text: 'Modal <span class="VPBadge tip">2024.1.1</span>', link: 'modal'},
-                        {text: 'Tooltip <span class="VPBadge tip">2024.1.1</span>', link: 'tooltip'},
-                        {text: 'Code <span class="VPBadge tip">2024.1.1</span>', link: 'code'},
-                        {text: 'Alert <span class="VPBadge tip">2024.1.2</span>', link: 'alert'},
-                        {text: 'Progress <span class="VPBadge tip">2024.1.2</span>', link: 'progress'},
-                        {text: 'Drawer <span class="VPBadge tip">2024.1.2</span>', link: 'drawer'},
-                        {text: 'Message <span class="VPBadge tip">2024.1.2</span>', link: 'message'},
-                        {text: 'Table <span class="VPBadge tip">2024.1.2</span>', link: 'table'},
-                        {text: 'Spin <span class="VPBadge tip">2024.2.0</span>', link: 'spin'},
-                        {text: 'Timeline <span class="VPBadge tip">2024.2.0</span>', link: 'timeline'},
-                        {text: 'Trend <span class="VPBadge tip">2024.2.0</span>', link: 'trend'},
-                        {text: 'Exception <span class="VPBadge tip">2024.2.0</span>', link: 'exception'},
-                        {text: 'Tag <span class="VPBadge tip">2024.2.0</span>', link: 'tag'},
-                        {text: 'Tree <span class="VPBadge tip">2024.3.0</span>', link: 'tree'},
-                        {text: 'Hover Card <span class="VPBadge tip">2024.4.0</span>', link: 'hover-card'},
-                        {text: 'Logger <span class="VPBadge tip">2024.5.1</span>', link: 'logger'},
-                        {text: 'Scrollbar <span class="VPBadge tip">2024.5.1</span>', link: 'scrollbar'},
-                        {text: 'Count Down <span class="VPBadge tip">2024.5.1</span>', link: 'count-down'},
-                        {text: 'Empty <span class="VPBadge tip">2024.5.1</span>', link: 'empty'},
-                        {text: 'Back Top <span class="VPBadge tip">2024.5.1</span>', link: 'back-top'},
-                        {text: 'Loading Bar <span class="VPBadge tip">2024.5.2</span>', link: 'view/loading-bar'},
-                        {text: 'Float Button <span class="VPBadge tip">2024.5.2</span>', link: 'view/float-button'},
-                        {text: 'QrCode <span class="VPBadge tip">2024.5.3</span>', link: 'view/qr-code'},
+                        {text: 'Modal', link: 'view/modal', icon: '/components/view/modal.svg', version: '2024.1.1'},
+                        {text: 'Tooltip', link: 'view/tooltip', icon: '/components/view/tooltip.svg', version: '2024.1.1'},
+                        {text: 'Code', link: 'view/code', icon: '/components/view/code.svg', version: '2024.1.1'},
+                        {text: 'Alert', link: 'view/alert', icon: '/components/view/alert.svg', version: '2024.1.2'},
+                        {text: 'Progress', link: 'view/progress', icon: '/components/view/progress.svg', version: '2024.1.2'},
+                        {text: 'Drawer', link: 'view/drawer', icon: '/components/view/drawer.svg', version: '2024.1.2'},
+                        {text: 'Message', link: 'view/message', icon: '/components/view/message.svg', version: '2024.1.2'},
+                        {text: 'Table', link: 'view/table', icon: '/components/view/table.svg', version: '2024.1.2'},
+                        {text: 'Spin', link: 'view/spin', icon: '/components/view/spin.svg', version: '2024.2.0'},
+                        {text: 'Timeline', link: 'view/timeline', icon: '/components/view/timeline.svg', version: '2024.2.0'},
+                        {text: 'Trend', link: 'view/trend', icon: '/components/view/trend.svg', version: '2024.2.0'},
+                        {text: 'Exception', link: 'view/exception', icon: '/components/view/exception.svg', version: '2024.2.0'},
+                        {text: 'Tag', link: 'view/tag', icon: '/components/view/tag.svg', version: '2024.2.0'},
+                        {text: 'Tree', link: 'view/tree', icon: '/components/view/tree.svg', version: '2024.3.0'},
+                        {text: 'Hover Card', link: 'view/hover-card', icon: '/components/view/hover-card.svg', version: '2024.4.0'},
+                        {text: 'Logger', link: 'view/logger', icon: '/components/view/logger.svg', version: '2024.5.1'},
+                        {text: 'Scrollbar', link: 'view/scrollbar', icon: '/components/view/scrollbar.svg', version: '2024.5.1'},
+                        {text: 'Count Down', link: 'view/count-down', icon: '/components/view/count-down.svg', version: '2024.5.1'},
+                        {text: 'Empty', link: 'view/empty', icon: '/components/view/empty.svg', version: '2024.5.1'},
+                        {text: 'Back Top', link: 'view/back-top', icon: '/components/view/back-top.svg', version: '2024.5.1'},
+                        {text: 'Loading Bar', link: 'view/loading-bar', icon: '/components/view/loading-bar.svg', version: '2024.5.2'},
+                        {text: 'Float Button', link: 'view/float-button', icon: '/components/view/float-button.svg', version: '2024.5.2'},
+                        {text: 'QrCode', link: 'view/qr-code', icon: '/components/view/qrcode.svg', version: '2024.5.3'}
                     ]
+
                     return {
                         text: `View [ ${items.length} ]`,
                         base: '/components/',
                         collapsed: false,
-                        items: items
+                        items: items.map(item => createSidebarItem(item))
                     };
                 })(),
                 (() => {
                     const items = [
-                        {text: 'Input <span class="VPBadge tip">2024.1.1</span>', link: 'input'},
-                        {text: 'Switch <span class="VPBadge tip">2024.1.2</span>', link: 'switch'},
-                        {text: 'Radio <span class="VPBadge tip">2024.1.2</span>', link: 'radio'},
-                        {text: 'Checkbox <span class="VPBadge tip">2024.1.2</span>', link: 'checkbox'},
-                        {text: 'Select <span class="VPBadge tip">2024.1.2</span>', link: 'select'},
-                        {text: 'Rate <span class="VPBadge tip">2024.1.2</span>', link: 'rate'},
-                        {text: 'Form <span class="VPBadge tip">2024.2.0</span>', link: 'form'},
-                        {text: 'Number <span class="VPBadge tip">2024.3.0</span>', link: 'number'},
-                        {text: 'Slider <span class="VPBadge tip">2024.2.0</span>', link: 'slider'},
-                        {text: 'Toggle <span class="VPBadge tip">2024.4.0</span>', link: 'toggle'},
-                        {text: 'Upload <span class="VPBadge tip">2024.5.0</span>', link: 'upload'},
-                        {text: 'Input Tag <span class="VPBadge tip">2024.5.2</span>', link: 'form/input-tag'},
-                        {text: 'Map <span class="VPBadge tip">2024.5.2</span>', link: 'form/map'},
+                        {text: 'Input', link: 'form/input', icon: '/components/form/input.svg', version: '2024.1.1'},
+                        {text: 'Switch', link: 'form/switch', icon: '/components/form/switch.svg', version: '2024.1.2'},
+                        {text: 'Radio', link: 'form/radio', icon: '/components/form/radio.svg', version: '2024.1.2'},
+                        {text: 'Checkbox', link: 'form/checkbox', icon: '/components/form/checkbox.svg', version: '2024.1.2'},
+                        {text: 'Select', link: 'form/select', icon: '/components/form/select.svg', version: '2024.1.2'},
+                        {text: 'Rate', link: 'form/rate', icon: '/components/form/rate.svg', version: '2024.1.2'},
+                        {text: 'Form', link: 'form/form', icon: '/components/form/form.svg', version: '2024.2.0'},
+                        {text: 'Slider', link: 'form/slider', icon: '/components/form/slider.svg', version: '2024.2.0'},
+                        {text: 'Number', link: 'form/number', icon: '/components/form/number.svg', version: '2024.3.0'},
+                        {text: 'Toggle', link: 'form/toggle', icon: '/components/form/toggle.svg', version: '2024.4.0'},
+                        {text: 'Upload', link: 'form/upload', icon: '/components/form/upload.svg', version: '2024.5.0'},
+                        {text: 'Input Tag', link: 'form/input-tag', icon: '/components/form/input-tag.svg', version: '2024.5.2'},
+                        {text: 'Map', link: 'form/map', icon: '/components/form/map.svg', version: '2024.5.2'},
                     ]
+
                     return {
                         text: `Form [ ${items.length} ]`,
                         base: '/components/',
                         collapsed: false,
-                        items: items
+                        items: items.map(item => createSidebarItem(item))
                     };
                 })(),
                 (() => {
                     const items = [
-                        {text: 'Tabs <span class="VPBadge tip">2024.1.2</span>', link: 'tabs'},
-                        {text: 'Pagination <span class="VPBadge tip">2024.1.2</span>', link: 'pagination'},
-                        {text: 'Menu <span class="VPBadge tip">2024.1.2</span>', link: 'menu'},
-                        {text: 'Badge <span class="VPBadge tip">2024.1.2</span>', link: 'badge'},
-                        {text: 'Breadcrumb <span class="VPBadge tip">2024.2.0</span>', link: 'breadcrumb'},
-                        {text: 'Link <span class="VPBadge tip">2024.2.0</span>', link: 'link'},
-                        {text: 'Dropdown <span class="VPBadge tip">2024.2.0</span>', link: 'dropdown'},
-                        {text: 'Contextmenu <span class="VPBadge tip">2024.4.0</span>', link: 'contextmenu'},
+                        {text: 'Tabs', link: 'navigation/tabs', icon: '/components/navigation/tabs.svg', version: '2024.1.2'},
+                        {text: 'Pagination', link: 'navigation/pagination', icon: '/components/navigation/pagination.svg', version: '2024.1.2'},
+                        {text: 'Menu', link: 'navigation/menu', icon: '/components/navigation/menu.svg', version: '2024.1.2'},
+                        {text: 'Badge', link: 'navigation/badge', icon: '/components/navigation/badge.svg', version: '2024.1.2'},
+                        {text: 'Breadcrumb', link: 'navigation/breadcrumb', icon: '/components/navigation/breadcrumb.svg', version: '2024.2.0'},
+                        {text: 'Link', link: 'navigation/link', icon: '/components/navigation/link.svg', version: '2024.2.0'},
+                        {text: 'Dropdown', link: 'navigation/dropdown', icon: '/components/navigation/dropdown.svg', version: '2024.2.0'},
+                        {text: 'Contextmenu', link: 'navigation/contextmenu', icon: '/components/navigation/contextmenu.svg', version: '2024.4.0'},
                     ]
+
                     return {
                         text: `Navigation [ ${items.length} ]`,
                         base: '/components/',
                         collapsed: false,
-                        items: items
+                        items: items.map(item => createSidebarItem(item))
                     };
                 })(),
                 (() => {
                     const items = [
-                        {text: 'Data Builder <span class="VPBadge tip">2024.5.0</span>', link: 'data-builder'},
-                        {text: 'Workflow <span class="VPBadge tip">2024.5.1</span>', link: 'workflow'},
-                        {text: 'Data Filter <span class="VPBadge tip">2024.5.2</span>', link: 'data/data-filter'},
+                        {text: 'Data Builder', link: 'data/data-builder', icon: '/components/data/data-builder.svg', version: '2024.5.0'},
+                        {text: 'Workflow', link: 'data/workflow', icon: '/components/data/workflow.svg', version: '2024.5.1'},
+                        {text: 'Data Filter', link: 'data/data-filter', icon: '/components/data/data-filter.svg', version: '2024.5.2'},
                     ]
+
                     return {
                         text: `Data [ ${items.length} ]`,
                         base: '/components/',
                         collapsed: false,
-                        items: items
+                        items: items.map(item => createSidebarItem(item))
                     };
                 })(),
             ]
