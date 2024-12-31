47cd6f3f3177f3c62d88e1e7d2655c24ec3485cd
qianmoQ <shicheng@devlive.org>
2024-12-31 10:52:38 +0800

    fix(code editor): fix search panel position

--- src/App.vue
+++ src/App.vue
@@ -2,43 +2,20 @@
   <div class="p-4 min-h-screen space-y-6">
     <h2 class="text-xl font-semibold mb-4">Shadcn Code Editor</h2>
     <ShadcnCodeEditor v-model="value"
-                      :auto-complete-config="{
-                          endpoint: 'http://jsonplaceholder.typicode.com/posts',
-                          method: 'GET',
-                          trigger: ['.', '@'],
-                          transform: (data: any) => {
-                            return data.map((item: any) => ({
-                              label: item.title,
-                              icon: 'Save',
-                              insertText: item.body,
-                              detail: item.title
-                            }))
-                          },
-                          // requestParams: (context) => ({
-                          //     word: context.word,
-                          //     line: context.position.lineNumber.toString()
-                          // }),
-                          // requestBody: (context) => ({
-                          //     code: context.modelValue,
-                          //     position: context.position
-                          // }),
-                          timeout: 3000,
-                          maxSuggestions: 10
-                        }"
-                      :context-menu-config="{
-                           showDefaultItems: true,
-                           items: [{
-                              label: 'Format Code',
-                              icon: 'Save',
-                              action: ({ editor, selection}) => {
-                                console.log(editor.getModel()?.getValueInRange(selection))
-                              }
-                           }]
-                      }"
                       :search-config="{
                           showHistory: true
-                      }"
-    />
+                      }">
+    </ShadcnCodeEditor>
+
+    <ShadcnCodeEditor v-model="value">
+    </ShadcnCodeEditor>
+
+    <ShadcnCodeEditor v-model="value"
+                      :search-config="{
+                            caseSensitive: false,
+                            replace: true
+                      }">
+    </ShadcnCodeEditor>
   </div>
 </template>
 
