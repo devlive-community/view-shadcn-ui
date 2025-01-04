b73165f00aa828f50a325dc7bd5c5c217b9ef3a2
qianmoQ <shicheng@devlive.org>
2025-01-04 23:01:12 +0800

    feat(mention): 添加展开关闭动画效果

--- src/ui/mention/ShadcnMention.vue
+++ src/ui/mention/ShadcnMention.vue
@@ -22,17 +22,25 @@
       </div>
     </div>
 
-    <div v-if="showItems && filteredItems.length > 0"
-         class="absolute z-50 w-full max-h-[200px] mt-1 overflow-auto bg-white border rounded-md shadow-lg">
-      <div v-for="(item, index) in filteredItems"
-           :key="item.id"
-           class="p-2 hover:bg-gray-100 cursor-pointer"
-           :class="{ 'bg-gray-100': selectedIndex === index }"
-           @click="(event) => selectItem(item, event)"
-           @mouseenter="selectedIndex = index">
-        {{ item.name }}
+    <Transition
+        enter-active-class="transition ease-out duration-200"
+        enter-from-class="opacity-0 translate-y-1"
+        enter-to-class="opacity-100 translate-y-0"
+        leave-active-class="transition ease-in duration-150"
+        leave-from-class="opacity-100 translate-y-0"
+        leave-to-class="opacity-0 translate-y-1">
+      <div v-if="showItems && filteredItems.length > 0"
+           class="absolute z-50 w-full max-h-[200px] mt-1 overflow-auto bg-white border rounded-md shadow-lg">
+        <div v-for="(item, index) in filteredItems"
+             :key="item.id"
+             class="p-2 hover:bg-gray-100 cursor-pointer"
+             :class="{ 'bg-gray-100': selectedIndex === index }"
+             @click="(event) => selectItem(item, event)"
+             @mouseenter="selectedIndex = index">
+          {{ item.name }}
+        </div>
       </div>
-    </div>
+    </Transition>
   </div>
 </template>
 
