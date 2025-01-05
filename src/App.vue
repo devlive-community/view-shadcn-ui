94b566d8337ac76d6ca002ecf462bba8a8c16224
qianmoQ <shicheng@devlive.org>
2025-01-05 11:04:48 +0800

    fix(input): 修复设置 prefix 和 suffix 导致 placeholder 位置不正确

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="p-32 space-y-7">
+    <ShadcnInput name="username" placeholder="请输入用户名">
+      <template #prefix>
+        <ShadcnIcon icon="User" size="18"/>
+      </template>
+
+      <template #suffix>
+        <ShadcnIcon icon="Search" size="18"/>
+      </template>
+    </ShadcnInput>
+
     <ShadcnImage src="https://view-shadcn-ui.devlive.org/logo.svg" alt="View Shadcn UI" />
   </div>
 </template>
