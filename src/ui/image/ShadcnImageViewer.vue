166b180e4a83bd57cc0b0bde3a0ebfc7fd5d128c
qianmoQ <shicheng@devlive.org>
2025-01-05 15:22:55 +0800

    feat(image viewer): 添加切换动态效果

--- src/ui/image/ShadcnImageViewer.vue
+++ src/ui/image/ShadcnImageViewer.vue
@@ -1,34 +1,36 @@
 <template>
   <div v-if="visible"
-       class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75 select-none"
+       class="fixed inset-0 z-50 flex items-center justify-center bg-black/75 select-none transition-opacity duration-300"
        @click.self="handleClose">
     <div class="relative w-full h-full flex items-center justify-center">
-      <button class="absolute top-4 right-4 text-white p-2 rounded-full hover:bg-gray-700 items-center flex"
+      <button class="absolute top-4 right-4 text-white p-2 rounded-full hover:bg-gray-500 items-center flex transition-colors duration-200"
               @click="handleClose">
         <ShadcnIcon icon="X"/>
       </button>
 
       <button v-if="current > 0"
-              class="absolute left-4 text-white p-2 rounded-full hover:bg-gray-700 items-center flex"
+              class="absolute left-4 text-white p-2 rounded-full hover:bg-gray-500 items-center flex transition-colors duration-200"
               @click="handlePrevious">
         <ShadcnIcon icon="ChevronLeft"/>
       </button>
 
       <button v-if="current < images.length - 1"
-              class="absolute right-4 text-white p-2 rounded-full hover:bg-gray-700 items-center flex"
+              class="absolute right-4 text-white p-2 rounded-full hover:bg-gray-500 items-center flex transition-colors duration-200"
               @click="handleNext">
         <ShadcnIcon icon="ChevronRight"/>
       </button>
 
       <div class="max-w-4xl max-h-full p-4">
-        <ShadcnImage :src="images[current].src"
-                     :alt="images[current].alt"
-                     :width="images[current].width"
-                     :height="images[current].height"
-                     :fit="images[current].fit"
-                     :lazy="images[current].lazy"
-                     :border="images[current].border">
-        </ShadcnImage>
+        <div class="relative">
+          <img v-for="(image, index) in images"
+               :key="index"
+               :src="image.src"
+               :alt="image.alt"
+               class="max-w-full max-h-[80vh] object-contain absolute top-0 left-0 transition-opacity duration-300"
+               :class="[
+                 index === current ? 'opacity-100 relative' : 'opacity-0'
+               ]"/>
+        </div>
       </div>
 
       <div class="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-white">
@@ -40,7 +42,6 @@
 
 <script setup lang="ts">
 import { ImagePreviewEmits, ImagePreviewProps } from './types'
-import ShadcnImage from './ShadcnImage.vue'
 
 const props = defineProps<ImagePreviewProps>()
 const emit = defineEmits<ImagePreviewEmits>()
