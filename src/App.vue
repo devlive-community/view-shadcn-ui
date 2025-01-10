483ffb0c3ab819563ca04657ed858ffe576adacb
qianmoQ <shicheng@devlive.org>
2025-01-10 22:51:48 +0800

    组件(按钮): 修复受父容器影响导致变形问题

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,16 @@
 <template>
   <div class="p-32 space-y-7 space-x-4">
     <div class="p-2 flex flex-col space-y-4 min-h-32 space-x-6 overflow-x-auto">
-      当前选择的值 {{ value }}
-      <ShadcnTree v-model="value" :data="data"/>
+      <ShadcnButton type="primary">Primary</ShadcnButton>
+      <ShadcnButton type="success">Success</ShadcnButton>
+      <ShadcnButton type="warning">Warning</ShadcnButton>
+      <ShadcnButton type="danger">Danger</ShadcnButton>
+      <ShadcnButton type="info">Info</ShadcnButton>
+      <ShadcnButton type="text">Text</ShadcnButton>
+      <ShadcnButton type="default">Default</ShadcnButton>
     </div>
   </div>
 </template>
 
-<script setup>
-import { ref } from "vue"
-
-const value = ref([])
-const data = [
-  {
-    value: 1,
-    label: 'Parent Node 1',
-    children: [
-      {value: 2, label: 'Child Node 1.1'},
-      {
-        value: 3,
-        label: 'Child Node 1.2',
-        children: [
-          {value: 4, selected: true, label: 'Child Node 1.2.1'}
-        ]
-      }
-    ]
-  },
-  {
-    value: 5,
-    label: 'Parent Node 2',
-    children: [
-      {value: 6, label: 'Child Node 2.1'}
-    ]
-  }
-]
+<script setup lang="ts">
 </script>
